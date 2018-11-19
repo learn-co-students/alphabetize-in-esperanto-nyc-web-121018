61fdc540827105aedc6db2749eb2f5de4edc15e5
tfuquirk <timothyfquirk@gmail.com>
2018-11-19 12:29:59 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
 def alphabetize(arr)
-  # code here
+  estperanto_alaphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alphabet_sorted = estperanto_alaphabet.split('')
+
+  arr.sort_by do |word|
+    word.split('').map do |letter|
+      estperanto_alaphabet.index(letter)
+    end
+  end
+  
 end

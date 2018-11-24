fa12f35c15f3c534815ea9923ef790c6f039a69e
github username <github email address>
2018-11-24 16:21:05 -0500

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  arr.sort_by do|sentence|
+    sentence.split('').map do|letter|
+      esperanto_alphabet.index(letter)
+
+    end
+
+  end
+
+
 end

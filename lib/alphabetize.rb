146233bbd744d551b6b3b184033c962c22d5236b
github username <github email address>
2018-11-30 12:09:47 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+    
+    arr.sort_by do |sentence|
+        sentence.split("").collect do |letter|
+            alphabet.index(letter)
+        end
+    end
+    
 end
